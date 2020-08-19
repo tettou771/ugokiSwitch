1c1e5c730e90b1c624a4f7df79be5f136ae9051a
Toru Takata <tettou771@gmail.com>
2020-08-19 23:34:53 +0900

    hide console (for windows)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include "ofMain.h"
 #include "ofApp.h"
 
+// hide console
+#ifdef WIN32
+#ifndef _DEBUG
+#pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
+#endif
+#endif
+
 //========================================================================
 int main( ){
 	ofSetupOpenGL(640, 480, OF_WINDOW);
